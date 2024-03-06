4785f676bf978d88895ec5c709608655ab65cfe4
eurico77 <euricomagalhaes61@gmail.com>
2024-03-06 10:28:00 -0300

    Add function to disable drag and drop

---
+++ index.js
@@ -0,0 +1,16 @@
+(function () {
+  function desativarDragDrop() {
+    document.addEventListener(
+      "dragstart",
+      (evento) => evento.preventDefault(),
+      false
+    );
+    document.addEventListener(
+      "drop",
+      (evento) => evento.preventDefault(),
+      false
+    );
+  }
+
+  desativarDragDrop();
+})();
